1eb886db2234c90faa79399a3cb93232fc4418f1
Tom James <794599429@qq.com>
2018-06-26 23:45:25 +0800

    second commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -14,12 +13,14 @@ def run_game():
     ship = Ship(ai_settings,screen)
 
     bullets = Group()
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
 
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets) 
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets) 
 
 run_game()
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings():
 
     def __init__(self):
 
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 900
+        self.screen_height = 600
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
         self.bullet_speed_factor = 1
