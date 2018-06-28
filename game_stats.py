90b2bfe3c07a3980bb1fb6b1993b8d064a0d81ab
宋豪斌 <shb@cjkt.com>
2018-06-28 18:14:33 +0800

    fifth commit

--- game_stats.py
+++ game_stats.py
@@ -1,9 +1,14 @@
+import game_functions as gf
+
 class GameStats():
     
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
         self.game_active = False
+        self.high_score = gf.read_high_score()
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
